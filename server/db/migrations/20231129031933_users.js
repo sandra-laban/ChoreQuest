2bf0e3b7ca6f84fa08f0560d223b1f6d987e7d41
Matt Spriggs <matt.c.spriggs@gmail.com>
2023-11-29 16:28:30 +1300

    Added migration for users table

---
+++ server/db/migrations/20231129031933_users.js
@@ -0,0 +1,14 @@
+export async function up(knex) {
+  return knex.schema.createTable('users', (table) => {
+    table.increments('id').primary
+    table.number('family_id')
+    table.string('auth_id')
+    table.string('name')
+    table.string('user_type')
+    table.number('current_points')
+  })
+}
+
+export async function down(knex) {
+  return knex.schema.dropTable('games')
+}
