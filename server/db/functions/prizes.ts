607af111dc0156a1e8022edb5791f644046602f0
Matt Spriggs <matt.c.spriggs@gmail.com>
2023-12-01 11:42:30 +1300

    Working on prizes

--- server/db/functions/prizes.ts
+++ server/db/functions/prizes.ts
@@ -1,15 +1,14 @@
 import { Prizes } from '../../../models/prizes'
 import db from '../connection'
 
-export function getAllPrizes(familyId: number): Promise<Prizes[]> {
-  return db('prizes')
-    .select([
-      'id',
-      'name',
-      'family_id as familyId',
-      'definition',
-      'price',
-      'quantity',
-    ])
-    .where('family_id', familyId)
+export function getAllPrizes(): Promise<Prizes[]> {
+  return db('prizes').select([
+    'id',
+    'name',
+    'family_id as familyId',
+    'definition',
+    'price',
+    'quantity',
+  ])
+  // .where('family_id', familyId)
 }
--- server/routes/prizes.ts
+++ server/routes/prizes.ts
@@ -4,11 +4,11 @@ import * as db from '../db/functions/prizes'
 const router = express.Router()
 
 // GET /api/v1/prizes
-router.get('/:id', async (req, res) => {
+router.get('/', async (req, res) => {
   try {
-    const { id } = req.params
-    const prizes = await db.getAllPrizes(Number(id))
-    res.json({ prizes })
+    // const { id } = req.params
+    const prizes = await db.getAllPrizes()
+    res.status(200).json(prizes)
   } catch (error) {
     res.sendStatus(500).json({ message: 'Unable to get prizes' })
   }
