4ab358270d209161bf259717330a89f593b2d4f6
Matt Spriggs <matt.c.spriggs@gmail.com>
2023-11-30 22:02:02 +1300

    Started work on prizes

--- server/routes/prizes.ts
+++ server/routes/prizes.ts
@@ -6,7 +6,9 @@ const router = express.Router()
 // GET /api/v1/prizes
 router.get('/:id', async (req, res) => {
   try {
-    const prizes = await db.getAllPrizes(id)
+    const { id } = req.params
+    const prizes = await db.getAllPrizes(Number(id))
+    res.json({ prizes })
   } catch (error) {
     res.sendStatus(500).json({ message: 'Unable to get prizes' })
   }
