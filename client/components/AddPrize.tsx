4f6b9c822e4b564321a0206e552d0f4415d2d2cb
Matt Spriggs <matt.c.spriggs@gmail.com>
2023-12-02 11:29:09 +1300

    Revising prizes to use familyId and using parameters

--- client/components/AddPrize.tsx
+++ client/components/AddPrize.tsx
@@ -1,8 +1,8 @@
-import { useAuth0 } from '@auth0/auth0-react'
-import {add}
+// import { useAuth0 } from '@auth0/auth0-react'
+// import {add}
 
 export default function AddPrize() {
-  const { user } = useAuth0()
-  console.log(user)
+  // const { user } = useAuth0()
+  // console.log(user)
   return <h2>Add a Prize!</h2>
 }
