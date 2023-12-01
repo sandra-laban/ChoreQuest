b5a5371145154a0ba2a3e822ec72a00f62003cf1
Sandra Laban <sandra.muncan@gmail.com>
2023-12-01 14:42:03 +1300

    fix datepicker

--- client/components/AddChoreForm.tsx
+++ client/components/AddChoreForm.tsx
@@ -30,17 +30,26 @@ const AddChore = () => {
     e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement | Date | null>
   ) {
     const { name, value } = e.target || { name: '', value: null }
-
+    //console.log('name, value', name, value)
     let newValue: string | null = null
 
     // If the input is the 'created' date, value will be a Date object or null
     if (name === 'created') {
+      console.log('value, type', value, typeof value)
       if (value instanceof Date) {
+        console.log('2')
         newValue = DateTime.fromJSDate(value).toISODate() || null
+        console.log('Luxon Formatted Date:', newValue)
+      } else if (typeof value === 'string') {
+        console.log('value is string')
+        newValue = value //Date.parse(value)
       } else if (value === null) {
+        console.log('3')
         newValue = null
       }
+      console.log('newValue', newValue)
     } else {
+      console.log('4')
       newValue = value as string | null
     }
 
@@ -51,24 +60,26 @@ const AddChore = () => {
   function handleAddChange(e: ChangeEvent<HTMLFormElement>) {
     e.preventDefault()
     addChoreMutation.mutate(form)
+    console.log(form, typeof form.created)
     setForm(initalForm)
+    console.log(initalForm, typeof initalForm.created)
   }
 
   return (
     <>
       <div>
-        <form action="" onSubmit={handleAddChange} aria-label="Add Chore Form">
+        <form
+          action=""
+          onSubmit={handleAddChange}
+          aria-label="Add Chore Form"
+          className="flex flex-col items-center justify-center"
+        >
           <div className="border p-8 border-gray-900/10 m-8">
-            <h2 className="text-base font-semibold leading-7 text-gray-900">
-              Add new chore
-            </h2>
+            <h2 className="mx-auto mt-12 mb-6 text-center">Add new chore</h2>
             <div className="space-y-12">
               <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                 <div className="sm:col-span-4">
-                  <label
-                    htmlFor="name"
-                    className="block text-sm font-medium leading-6 text-gray-900"
-                  >
+                  <label htmlFor="name" className="text-2xl">
                     Name:
                   </label>
                   <input
@@ -76,15 +87,12 @@ const AddChore = () => {
                     onChange={handleChange}
                     name="name"
                     value={form.name}
-                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                    className="m-4 border-solid border-2 border-black p-2 px-5 rounded-lg mb-12"
                   ></input>
                 </div>
               </div>
               <div className="sm:col-span-4">
-                <label
-                  htmlFor="points"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
+                <label htmlFor="points" className="text-2xl">
                   Points:
                 </label>
                 <input
@@ -92,7 +100,7 @@ const AddChore = () => {
                   onChange={handleChange}
                   name="points"
                   value={form.points}
-                  className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                  className="m-4 border-solid border-2 border-black p-2 px-5 rounded-lg mb-12"
                 ></input>
               </div>
               <div className="form-group">
@@ -100,15 +108,18 @@ const AddChore = () => {
                   id="created"
                   selected={DateTime.fromISO(form.created).toJSDate()} // Convert Luxon date to JavaScript Date
                   onChange={(date) => {
-                    const isoDate = DateTime.fromJSDate(date).toISODate()
-                    handleChange({
-                      target: { name: 'created', value: isoDate },
-                    } as ChangeEvent<HTMLInputElement>)
+                    if (date) {
+                      const isoDate = DateTime.fromJSDate(date).toISODate()
+                      console.log('Luxon Formatted Date:', isoDate)
+                      handleChange({
+                        target: { name: 'created', value: isoDate },
+                      } as ChangeEvent<HTMLInputElement>)
+                    }
                   }}
                   dateFormat="yyyy-MM-dd"
                 />
               </div>
-              <button className="rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+              <button className="btn-primary mb-12 items-center justify-center">
                 Add Chore
               </button>
             </div>
