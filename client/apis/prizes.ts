5100763620233450ffa4d9ce77b35e19f8d07bd9
Matt Spriggs <matt.c.spriggs@gmail.com>
2023-12-01 22:33:23 +1300

    Setting up add prize form

--- client/apis/prizes.ts
+++ client/apis/prizes.ts
@@ -1,8 +1,14 @@
 import request from 'superagent'
-import { Prizes } from '../../models/prizes'
+import { PrizeData, Prizes } from '../../models/prizes'
 
 const serverUrl = '/api/v1/prizes'
+
 export async function getAllPrizes(): Promise<Prizes[]> {
   const response = await request.get(`${serverUrl}`)
   return response.body
+}
+
+export async function addPrize(newPrize: PrizeData): Promise<Prizes> {
+  const response = await request.post('/api/v1/prizes').send(newPrize)
+  return response.body.prizes
 }
--- client/components/AddPrize.tsx
+++ client/components/AddPrize.tsx
@@ -1,4 +1,5 @@
 import { useAuth0 } from '@auth0/auth0-react'
+import {add}
 
 export default function AddPrize() {
   const { user } = useAuth0()
