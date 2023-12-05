2d446cf3caf470ae89e102af4f5e00f2e7493bfd
Matt Spriggs <matt.c.spriggs@gmail.com>
2023-12-05 23:54:50 +1300

    Working on add prize form

--- client/components/AddPrize.tsx
+++ client/components/AddPrize.tsx
@@ -4,10 +4,10 @@ import { PrizeData } from '../../models/prizes'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { ChangeEvent, useState } from 'react'
 
-export default function AddPrize() {
-  interface Props {
-    setFormView: (value: boolean) => void
-  }
+interface Props {
+  setFormView: (value: boolean) => void
+}
+export default function AddPrize({ setFormView }: Props) {
   // const { user } = useAuth0()
   // console.log(user)
   return <h1>PLACEHOLDER FOR ADD PRIZE FORM</h1>
