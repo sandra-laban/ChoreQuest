c99434c99e5f98c0823c3082ff1f6865ec7251d3
Sentuar <thayward2001@gmail.com>
2023-12-06 22:56:11 +1300

    added update on unasginging

--- client/components/Chore.tsx
+++ client/components/Chore.tsx
@@ -211,6 +211,12 @@ function ChoreBox({ chore, completed }: Props) {
       queryClient.invalidateQueries({ queryKey: ['chores'] })
       queryClient.invalidateQueries({ queryKey: ['chorelist'] })
       queryClient.invalidateQueries({ queryKey: ['profile'] })
+      socketInstance.emit('update_query_key', {
+        queryKey: ['notifications', 'chores', 'profile', 'chorelist'],
+        users: 'family',
+        notificationMessage: null,
+        pageUrl: null,
+      })
     },
   })
 
